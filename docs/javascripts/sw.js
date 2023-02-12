aedd0c38bcb3cad04c6d7999daa959718dfa364f
virustotalop <virustotalop@gmail.com>
2023-02-11 18:17:56 -0800

    Attempt to register service worker correctly

--- docs/javascripts/sw.js
+++ docs/javascripts/sw.js
@@ -1,19 +1,18 @@
 // This is the "Offline copy of pages" service worker
-//Used https://www.pwabuilder.com to generate
-const CACHE = "pwabuilder-offline";
+//Used https://www.pwabuilder.com 
+const CACHE = "blis-online";
 
-import('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js').then(workbox => {
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-    self.addEventListener("message", (event) => {
-        if (event.data && event.data.type === "SKIP_WAITING") {
-            self.skipWaiting();
-        }
-    });
-
-    workbox.routing.registerRoute(
-        new RegExp('/*'),
-        new workbox.strategies.StaleWhileRevalidate({
-            cacheName: CACHE
-        })
-    );
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
+
+workbox.routing.registerRoute(
+  new RegExp('/*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
