93a030906e3c129909b7468df6775058f5a83f93
virustotalop <virustotalop@gmail.com>
2023-02-11 18:14:21 -0800

    Attempt to fix import

--- docs/javascripts/sw.js
+++ docs/javascripts/sw.js
@@ -2,17 +2,18 @@
 //Used https://www.pwabuilder.com to generate
 const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+import('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js').then(workbox => {
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+    self.addEventListener("message", (event) => {
+        if (event.data && event.data.type === "SKIP_WAITING") {
+            self.skipWaiting();
+        }
+    });
+
+    workbox.routing.registerRoute(
+        new RegExp('/*'),
+        new workbox.strategies.StaleWhileRevalidate({
+            cacheName: CACHE
+        })
+    );
 });
-
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
