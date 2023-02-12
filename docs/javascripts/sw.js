82503cc3f49c7448af1bea25e0ee9778a0c0fe39
virustotalop <virustotalop@gmail.com>
2023-02-11 19:22:24 -0800

    Attempt to use awesome pages

--- docs/javascripts/sw.js
+++ docs/javascripts/sw.js
@@ -4,8 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox
 
 const CACHE = "blis-online";
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "offline/offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
